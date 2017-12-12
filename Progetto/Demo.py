91d5d22f9e9a91ee5e8ae69005b06c78113053d2
Antonio <antonio.dorazio9@outlook.it>
2017-12-12 12:04:32 +0100

    relazione

--- Progetto/Demo.py
+++ Progetto/Demo.py
@@ -1,7 +1,9 @@
 from LazyDictionary import LazyDictionary
-
+import cProfile
+import pstats
 
 def main():
+    """Demo sull'esecuzione del dizionario"""
     print("registro = LazyDictionary()")
     registro = LazyDictionary()
 
@@ -30,7 +32,10 @@ def main():
     print(registro.tree.stampa())
 
     print("Inizializzo dizionario da struttura esistente")
+
+
     codiciLibri = [[13, "1984"], [9, "Harry Potter"], ["N.A.", "Il signore degli Anelli"]]
+
     libri = LazyDictionary(codiciLibri)
     print(libri.get(13))
     print(libri.get("N.A."))
@@ -49,5 +54,57 @@ def main():
     ciao.tree.stampa()
 
 
+
+
+### CODE PROFILING ###
+
+'''Variabile globale per il dizionario, a scopo di profiling'''
+registro = LazyDictionary()
+
+def creaDizionario():
+    registro = LazyDictionary()
+
+def popolaDizionario():
+    # Popolo dizionario
+    registro.add("Matematica", 8)
+    registro.add("Storia", 6.5)
+    registro.add("Scienze", 6.5)
+    registro.add("Fisica", 6.5)
+    registro.add("Geografia", 6.5)
+    registro.add("Informatica", 6.5)
+    registro.add("Arte", 6.5)
+    registro.add("Italiano", 6.5)
+    registro.add("Ed. Fisica", "Distinto")
+
+def stampaChiavi():
+    chiavi = registro.keys()
+    print ("LE CHIAVI SONO ", chiavi)
+def valori():
+    #Valori del dizionario
+    valori = registro.values()
+
+def coppie():
+    #Coppie [chiave, valore]
+    coppie = registro.allPairs()
+
+def rimuovi():
+    #Elimino un elemento
+    registro.remove("Storia")
+
+def albero():
+    # Stampo l'intero albero
+    print(registro.tree.stampa())
+
+def elemento():
+    # Stampo un preciso elemento
+    print(registro.get("Arte"))
+
+
 if __name__ == '__main__':
     main()
+
+    popolaDizionario()
+    cProfile.run('stampaChiavi()', "output.txt")
+    p = pstats.Stats("output.txt")
+    p.strip_dirs().sort_stats("time").print_stats()
+
